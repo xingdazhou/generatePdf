0e21a099ad4efa0961c4a33bfa1311b5a60b1422
zhouxd <183088418@qq.com>
2023-02-24 16:16:11 +0800

    fix : 修改 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,37 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: zhouxd
  * @Date: 2023-02-22 12:55:02
  * @LastEditors: zhouxd
- * @LastEditTime: 2023-02-23 16:47:27
+ * @LastEditTime: 2023-02-24 16:07:17
  */
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import * as path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
         //设置别名
         alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+            "@": path.resolve(__dirname, "src"),
+        },
     },
-    plugins: [vue()]
+    plugins: [vue()],
+    server: {
+        port: 8080, //启动端口
+        hmr: {
+            host: "127.0.0.1",
+            port: 8080,
+        },
+        // 设置 https 代理
+        proxy: {
+            "/api": {
+                target: "your https address",
+                changeOrigin: true,
+                rewrite: (path: string) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
 });
